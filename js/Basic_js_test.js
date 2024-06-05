b410b92f1acd10a09ea0eb4324993e862252ba07
admin <dbcksthd123@naver.com>
2024-06-05 13:56:15 +0900

    열심히 했다

---
+++ js/Basic_js_test.js
@@ -0,0 +1,45 @@
+/*var a = 'Master yi'
+var b = 'Master yi'
+var c = 'Master yi'
+var d = 'Master yi'
+var e = 'Master yi'
+var f = 'Master yi'
+var g = 'Master yi'
+var h = 'Master yi'
+var i = 'Master yi'
+var j = 'Master yi'
+var k = 'Master yi'
+var l = 'Master yi'
+var n = 'Master yi'
+var m = 'Master yi'
+var o = 'Master yi'
+var p = 'Master yi'
+var q = 'Master yi'
+var r = 'Master yi'
+var s = 'Master yi'
+var t = 'Master yi'
+var u = 'Master yi'
+var v = 'Master yi'
+var w = 'Master yi'
+var x = 'Master yi'
+var y = 'Master yi'
+var z = 'Master yi'
+*/
+
+/*주석이지만 마스터 이를 빼먹을 순 없지*/
+
+if (true) {
+    let c = 'let 접근';
+    var c_1 = 'var 접근';
+}
+//console.log(c); // Error?
+console.log(c_1);
+
+let d = 5;
+//let d = '값 재할당'; // Error?
+console.log(d);
+
+const e = '상수1 접근';
+//e = 5;
+//const f // Error?
+console.log(e);
